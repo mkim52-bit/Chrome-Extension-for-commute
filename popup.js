9bc7a547c02774ac6c1eaf928433f5161ac84274
mkim52-bit <70614305+mkim52-bit@users.noreply.github.com>
2023-01-30 06:05:18 -0500

    Added features like departure time, swap direction

--- popup.js
+++ popup.js
@@ -1,4 +1,7 @@
+
 function initMap() {
+  let swapDirections = false;
+  
   //renders the line for route
   const directionsRenderer = new google.maps.DirectionsRenderer({
     //changes line color
@@ -20,32 +23,88 @@ function initMap() {
   directionsRenderer.setMap(map);
 
   directionsRenderer.setPanel(document.getElementById("sidebar"));
+  
+  calculateAndDisplayRoute(directionsService, directionsRenderer, swapDirections);
   //when transit mode is changed searches direction
-  calculateAndDisplayRoute(directionsService, directionsRenderer);
   document.getElementById("mode").addEventListener("change", () => {
-    calculateAndDisplayRoute(directionsService, directionsRenderer);
+    calculateAndDisplayRoute(directionsService, directionsRenderer, swapDirections);
+  });
+  //when swap button is clicked searches direction
+  document.getElementById("swap").addEventListener("click", () => {
+    swapDirections = !swapDirections;
+    calculateAndDisplayRoute(directionsService, directionsRenderer, swapDirections);
+  });
+  document.getElementById("time").addEventListener("change", () => {
+    calculateAndDisplayRoute(directionsService, directionsRenderer, swapDirections);
   });
 }
 
-function calculateAndDisplayRoute(directionsService, directionsRenderer) {
+//Calculates departure time
+function calculateDate(){
+  let time = document.getElementById("time").value.split(':')
+  let hour = time[0]
+  let min = time[1]
+  let now = new Date();
+  
+  if(now.getHours() > hour){
+    if(now.getMinutes() >= min){
+      now.setDate(now.getDate() + 1)
+      now.setHours(hour)
+      now.setMinutes(min)
+      return now
+    }
+  }
+    
+    now.setHours(hour)
+    now.setMinutes(min) 
+
+  return now
+}
+
+function calculateAndDisplayRoute(directionsService, directionsRenderer, swapDirections) {
   const selectedMode = document.getElementById("mode").value;
   chrome.storage.sync.get(["work", "home"], function(obj){
-    if(obj != undefined){
+    if(swapDirections === false){
+     
       directionsService
         .route({
           origin: obj.home,
           destination: obj.work,
-          // Note that Javascript allows us to access the constant
-          // using square brackets and a string value as its
-          // "property."
-          travelMode: google.maps.TravelMode[selectedMode],
+          provideRouteAlternatives: true,
+          travelMode: selectedMode,
+          optimizeWaypoints:true,
+          transitOptions: {
+            departureTime: calculateDate(),
+            // routingPreference: 'LESS_WALKING'
+          },
+          unitSystem: google.maps.UnitSystem.IMPERIAL
+            
         })
         .then((response) => {
           directionsRenderer.setDirections(response);
         })
-        .catch((e) => window.alert("Directions request failed due to " + status));
+        .catch((e) => window.alert("Directions request failed due to " + e));
     
   }
+  else if(swapDirections == true){
+    directionsService
+    .route({
+      origin: obj.work,
+      destination: obj.home,
+      provideRouteAlternatives: true,
+      travelMode: selectedMode,
+      optimizeWaypoints:true,
+      transitOptions: {
+        departureTime: calculateDate(),
+        // routingPreference: 'LESS_WALKING'
+      },
+      unitSystem: google.maps.UnitSystem.IMPERIAL
+        
+    })
+    .then((response) => {
+      directionsRenderer.setDirections(response);
+    })
+    .catch((e) => window.alert("Directions request failed due to " + e));}
   
 })
 }
