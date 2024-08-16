c81e8717ab41d92d984be2d4e727772ca46d4b26
mkim52-bit <70614305+mkim52-bit@users.noreply.github.com>
2024-08-15 21:24:47 -0400

    deleting consol logs

--- eventPage.js
+++ eventPage.js
@@ -9,7 +9,6 @@ chrome.contextMenus.create(contextMenuItem)
 chrome.contextMenus.onClicked.addListener(function(clickData){
     if (clickData.menuItemId === "search" && clickData.selectionText) {
         //store the address highlighted
-        console.log("in")
         chrome.storage.sync.set({"home":clickData.selectionText})
     }
 })
